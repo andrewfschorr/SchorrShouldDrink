20825f5c3ac91e0fca5aeaff3783b93aa9be53fc
Andrew Schorr <afs35mm@gmail.com>
2019-07-17 22:11:56 -0400

    Added fetch

--- src/components/ingredient/index.tsx
+++ src/components/ingredient/index.tsx
@@ -8,17 +8,18 @@ const IngredientWrapper = styled.div`
 
 export default () => {
 
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState<Array<string>>([]);
 
   useEffect(() => {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').then(resp => resp.json())
-    .then(console.log);
+    .then((data: any) => data.drinks.map((item: any) => item.strIngredient1))
+    .then(setIngredients);
   }, []);
 
   return (
     <IngredientWrapper>
       <h1 className="text-2xl border-b-2 border-gray-600 ">Ingredients</h1>
-
+      {ingredients.length > 0 && ingredients.map(item => console.log(item))}
     </IngredientWrapper>
   );
 }
