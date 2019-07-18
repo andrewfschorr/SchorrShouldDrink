6cd3c58b2b84679059c4d2f7dd86e26ef9443a25
Andrew F Schorr <sschrr@amazon.com>
2019-07-18 18:42:03 -0400

    Updates

---
+++ precache-manifest.28dc07202599f7b0996afa95e5685b4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6e98596bb43c2d0e480a8203b073d6",
+    "url": "/SchorrShouldDrink/index.html"
+  },
+  {
+    "revision": "d1bfd75a5b1402909094",
+    "url": "/SchorrShouldDrink/static/css/main.fa6dd849.chunk.css"
+  },
+  {
+    "revision": "3afd9149000ae24b48b4",
+    "url": "/SchorrShouldDrink/static/js/2.52d9ec65.chunk.js"
+  },
+  {
+    "revision": "d1bfd75a5b1402909094",
+    "url": "/SchorrShouldDrink/static/js/main.0e0ca908.chunk.js"
+  },
+  {
+    "revision": "d4aa7f16ec6dfaf7d51e",
+    "url": "/SchorrShouldDrink/static/js/runtime~main.6260d859.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ShouldShouldDrink/precache-manifest.4a5ab9910aa946d405103150f1e86d21.js"
+  "/SchorrShouldDrink/precache-manifest.28dc07202599f7b0996afa95e5685b4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShouldShouldDrink/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SchorrShouldDrink/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
