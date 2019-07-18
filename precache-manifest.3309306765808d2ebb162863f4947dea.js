897e41db1e16e144498dcd7abf6f0ff6ca8edfaa
Andrew F Schorr <sschrr@amazon.com>
2019-07-18 16:52:57 -0400

    Updates

---
+++ precache-manifest.3309306765808d2ebb162863f4947dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e7db8859bf03dac90e2ffe14fe365d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "314426ae97eb975f1559",
+    "url": "/static/css/main.fa6dd849.chunk.css"
+  },
+  {
+    "revision": "ca491239a01182a860a8",
+    "url": "/static/js/2.fb28f5de.chunk.js"
+  },
+  {
+    "revision": "314426ae97eb975f1559",
+    "url": "/static/js/main.eac4bd5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
